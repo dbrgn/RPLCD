09607fda71f8bb44fdddf501153d7729e391fc11
Danilo Bargen <gezuru@gmail.com>
2013-05-11 03:07:36 +0200

    Added basic test loop

--- test.py
+++ test.py
@@ -1,19 +1,35 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division, absolute_import, unicode_literals
 
-import time
-
-import RPIO
-
-from RPLCD import CharLCD
+from RPLCD import CharLCD, Direction, CursorMode, ShiftMode
 
 
-#lcd = CharLCD(pin_rs=15, pin_e=16, pins_data=[21, 22, 23, 24], numbering_mode=RPIO.BOARD)
-lcd = CharLCD(pin_rs=15, pin_e=16, pins_data=[21, 22, 23, 24], numbering_mode=RPIO.BOARD)
-lcd.setup(cols=20, rows=4, dotsize=8)
-lcd.turn_on()
+lcd = CharLCD()
 
-for char in 'Hello world! Hello world! Hello world!':
-    lcd.write(ord(char))
+lcd.setup()
+raw_input('Display should be blank. ')
 
-lcd.close(clear=False)
+lcd.cursor_mode = CursorMode.blink
+raw_input('The cursor should now blink. ')
+
+lcd.cursor_mode = CursorMode.line
+raw_input('The cursor should now be a line. ')
+
+lcd.write_string('Hello world!')
+raw_input('"Hello world!" should be on the LCD. ')
+
+lcd.home()
+raw_input('Cursor should now be at initial position. ')
+
+lcd.cursor_pos = (1, 0)
+lcd.write_string('2')
+lcd.cursor_pos = (2, 0)
+lcd.write_string('3')
+lcd.cursor_pos = (3, 0)
+lcd.write_string('4')
+raw_input('Lines 2, 3 and 4 should now be labelled with the right numbers. ')
+
+lcd.clear()
+raw_input('Display should now be clear, cursor should be at initial position. ')
+
+#lcd.close()
