a97942fd9b72c53b9e1b511123f213b9ac079b24
Danilo Bargen <mail@dbrgn.ch>
2017-05-13 16:31:03 +0200

    Add RPLCD.__version__ attribute
    Fixes #50

--- RPLCD/__init__.py
+++ RPLCD/__init__.py
@@ -5,6 +5,9 @@ from .contextmanagers import cursor, cleared
 from .gpio import CharLCD as GpioCharLCD
 
 
+__version__ = '0.9.0'
+
+
 class CharLCD(GpioCharLCD):
     def __init__(self, *args, **kwargs):
         warnings.warn("Using RPLCD.CharLCD directly is deprecated. " +
