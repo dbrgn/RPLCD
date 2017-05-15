d4347d46542e22210afef2610868ff297a96ffc6
paul_enuta <paulenuta@gmail.com>
2017-05-15 08:21:01 +0300

    Add test configuration for 40x2 display - some correction and updates.
    Tested confirmed with 40x2 display (RAYSTAR - RC4002A-BIW-JSX) and i2c
    MCP23008 (Adafruit backpack custom made equivalent).
    [./test_40x2.py i2c MCP23008 0x20 A00]

--- test_40x2.py
+++ test_40x2.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Copyright (C) 2013-2017 Danilo Bargen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 import sys
@@ -17,11 +38,12 @@ def print_usage():
     print('Usage: %s i2c <expander> <addr> <charmap>' % sys.argv[0])
     print('       %s gpio <charmap>' % sys.argv[0])
     print('')
-    print('<addr>     The I²C address can be found with `i2cdetect 1` from the i2c-tools')
-    print('           package.')
-    print('<charmap>  Charmap can be either A00 or A02. If your display contains Japanese')
-    print('           characters, it probably uses the A00 charmap, otherwise A02.')
-    print('<expander> Supported expanders are PCF8574 and MCP23008')
+    print('<expander>  Supported I²C port expanders are PCF8574 and MCP23008')
+    print('<addr>      The I²C address (in hex format) can be found with')
+    print('            `i2cdetect 1` from the i2c-tools package.')
+    print('<charmap>   Charmap can be either A00 or A02. If your display contains Japanese')
+    print('            characters, it probably uses the A00 charmap, otherwise A02.')
+    print('')
     sys.exit(1)
 
 
@@ -35,7 +57,7 @@ if sys.argv[1] == 'i2c':
 elif sys.argv[1] == 'gpio':
     if len(sys.argv) != 3:
         print_usage()
-    lcd = gpio.CharLCD(cols=40, rows=2, charmap=sys.argv[2])
+    lcd = gpio.CharLCD(cols=16, rows=2, charmap=sys.argv[2])
 else:
     print_usage()
 
@@ -96,9 +118,9 @@ lcd.write_string('and Spam')
 lcd.display_enabled = True
 input('Display should now show "Eggs, Ham and Spam" with a line break after "Ham". ')
 
-lcd.shift_display(4)
+lcd.shift_display(20)
 input('Text should now be shifted to the right by 4 characters. ')
-lcd.shift_display(-4)
+lcd.shift_display(-20)
 input('Shift should now be undone. ')
 
 lcd.text_align_mode = Alignment.right
@@ -122,7 +144,7 @@ lcd.write_string('6')
 input('The numbers 1-6 should now be displayed in a zig zag line starting in the top left corner. ')
 
 lcd.clear()
-lcd.write_string('This text will wrap around both lines from 0,0 to 0,39 and 1,0 to 1,39')
+lcd.write_string('This text will wrap around both lines from column 0 to column 39')
 input('Text should nicely wrap around lines. ')
 
 lcd.clear()
@@ -146,12 +168,12 @@ lcd.write_string('\x00')
 input('Now both faces should be happy. ')
 
 lcd.clear()
-lcd.write_string('1234567890123456\r\n2nd line')
+lcd.write_string('1234567890abcdefghijklmnopqrstuvxyzABCDE\r\n2nd line')
 input('The first line should be filled with numbers, the second line should show "2nd line"')
 
 lcd.clear()
-lcd.write_string('9994567890123456789012345678901234567890\n\r\n123')
-input('The display should be "1234567890" 1to40')
+lcd.write_string('1234567890\n\r\n123')
+input('The display should show "1234567890" on the first line')
 
 lcd.clear()
 try:
@@ -160,3 +182,4 @@ except ValueError:
     pass
 lcd.close()
 print('Test done. If you have a backlight, it should now be off.')
+
