220839b51a47a680613200c14a86a5cd764d3444
Danilo Bargen <mail@dbrgn.ch>
2018-03-25 01:19:49 +0100

    Add API tests

---
+++ tests/test_api.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import, unicode_literals
+
+from RPLCD.gpio import CharLCD
+
+
+def test_missing_numbering_mode(mocker, charlcd_kwargs):
+    kwargs = {
+        'pin_rs': 15,
+        'pin_rw': 18,
+        'pin_e': 16,
+        'pins_data': [21, 22, 23, 24],
+    }
+    try:
+        CharLCD(**kwargs)
+    except ValueError as ex:
+        assert 'https://gist.github.com/dbrgn/77d984a822bfc9fddc844f67016d0f7e' in str(ex)
+    else:
+        assert False, 'ValueError not raised'
